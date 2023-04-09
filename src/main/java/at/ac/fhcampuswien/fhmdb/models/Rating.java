3bfd6efaf1ac5dbc3e915020f46f7a9e20139672
000Mousavi <seyedeh.zahra.mousavi@stud.fh-campuswien.ac.at>
2023-04-09 22:56:58 +0200

    Rating

--- src/main/java/at/ac/fhcampuswien/fhmdb/models/Rating.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/models/Rating.java
@@ -1,11 +1,16 @@
 package at.ac.fhcampuswien.fhmdb.models;
 
 public enum Rating {
+    RATING_0(0),
     RATING_1(1),
     RATING_2(2),
     RATING_3(3),
     RATING_4(4),
-    RATING_5(5);
+    RATING_5(5),
+    RATING_6(6),
+    RATING_7(7),
+    RATING_8(8),
+    RATING_9(9);
 
     private final int value;
 
